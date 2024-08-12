81168f84e6a82c0d78850fc97b8efbc036f23c54
darynafesyk <daryna.fesyk@stud.sundsgarden.se>
2024-08-12 17:32:56 +0200

    deployment erroe

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -65,7 +65,7 @@ const BookingForm = () => {
       setAvailableServices([]);
       setAvailableTimes([]);
     }
-  }, [startDate, bookedSlots, selectedDoctor]);
+  }, [startDate, bookedSlots, selectedDoctor, doctors]);
   
 
   const handleChange = (e) => {
