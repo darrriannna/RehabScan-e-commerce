2e656c7b71a7f18625c323a10ea9c6bfd83248b4
darynafesyk <daryna.fesyk@stud.sundsgarden.se>
2024-08-12 11:38:30 +0200

    changes main home

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,44 +1,74 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../styles/index.css'; // Import your custom CSS file
+import { NavLink } from 'react-router-dom';
 import Slideshow from './HomeSlide';
 
-
-
 const Home = () => {
-
   return (
     <div className="main">
-      
       <Slideshow />
+
       <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="card">
-              <img src="./assets/senior-couple-talking-with-their-doctor-while-he-is-showing-them-something-computer.jpg" className="card-img" alt="..."/>
-              <div className="card-body">
-                <h5 className="card-title">Get the appointment with a doctor now</h5>
-                <p className="card-text">
-                  På NO Kliniken träffar du ett team med naprapater och ortoped. Vårt omhändertagande av dig återskapar kroppens goda rörelse och funktion. Vi har ett etablerat kontaktnät
-                   av andra experter som anlitas när så behövs. Förutom behandlingar ger vi dig även individuella råd och rekommendationer. 
-                  Dessa kan handla om hållning, ergonomi, motion eller mer specifika rehabövningar. Detta gör resultatet än bättre och minimerar risken för återfall.
-                </p>
-                <a href="pages/book-form.html" className="btn">Book</a>
-              </div>
-            </div>
+        <h2 className="title-main">NO Pain Kliniken - vård i världsklass</h2>
+        <h3>
+        På vår privata klinik i Göteborg erbjuder vi naprapati och ortopediska tjänster för att möta dina hälsobehov.</h3>
+        <div className="about-link">
+          <a href="/AboutPage" className="about-anchor">
+            Läs mer <span className="about-arrow">→</span>
+          </a>
+        </div>
+      </div>
+
+          <div className='container'>
+      <div className='container-main-book'>
+        <img className='image-main' src="/assets/close-up-physiotherapist-massaging-patient.jpg" alt="min-home-books" />
+        <div className='content-right'>
+          <div className='col treatments'>
+            <h2>Har du ...</h2>
+            <p>- Nacke och ryggbesvär</p>
+            <p>- Diskbråck (Ländrygg och Nacke) </p>
+            <p>- Kronisk och oförklarlig smärta</p>
+            <p>- Huvudvärk & migrän</p>
+            <p>- Axel- och armbågssmärta </p>
+            <p>- Höft/bäcken/knä smärta</p>
+            <h2>?</h2>
           </div>
-          <div className="col">
-            <p className="title">NOVA - healthcare at its finest</p>
-            <p className="description">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam blanditiis molestiae enim dolor in odio sint provident eius, culpa magnam fugit nemo minus, eaque consectetur? Accusantium quis excepturi qui aspernatur.
-            </p>
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">MRI</h5>
-                <h6 className="card-subtitle">Get your MR results within hours</h6>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="card-link">Read More</a>
-                <a href="pages/book-form.html" className="card-link">Book Time</a>
+          <NavLink to="/bookappointment" className="btn btn-info m-2 fs-5">
+            <i></i>Boka tid 
+          </NavLink>
+        </div>
+      </div>
+    </div>
+
+
+      <div className='container'>
+        {/* New Container Section */}
+        <div className="services-container row">
+          <div className="col-md-6">
+            <h2 className='title-services'>Har du en privat sjukvårdsförsäkring?</h2>
+            <div className="partners-link">
+          <a href="/partners" className="partners-anchor">
+            Partners <span className="arrow">→</span>
+          </a>
+        </div>
+          </div>
+          <div className="col-md-6">
+            <div className="services-list">
+              <div className="service-item">
+                <i className="service-icon"></i>
+                <div>
+                  <h5>Naprapatbesök</h5>
+                  <p>Naprapatbehandling 750kr</p>
+                </div>
               </div>
+              <div className="service-item">
+                <i className="service-icon"></i>
+                <div>
+                  <h5>Läkarbesök</h5>
+                  <p> 1300 kr</p>
+                </div>
+              </div>
+              
             </div>
           </div>
         </div>
@@ -47,17 +77,4 @@ const Home = () => {
   );
 };
 
-// Function to handle carousel sliding
-const slide = (direction) => {
-  const items = document.querySelectorAll('.carousel-item');
-  const activeItem = document.querySelector('.carousel-item.active');
-  let index = Array.from(items).indexOf(activeItem);
-  items[index].classList.remove('active');
-  index = (index + direction + items.length) % items.length;
-  items[index].classList.add('active');
-};
-
 export default Home;
-
-
-
