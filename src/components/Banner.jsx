224a359ad2cb204a8de4210264668821317988e4
darynafesyk <daryna.fesyk@stud.sundsgarden.se>
2024-08-26 17:42:07 +0200

    changing timer on banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,30 +1,37 @@
 import React, { useEffect, useState } from 'react';
-
+import '../styles/index.css'; // Import the CSS file
 
 const Banner = () => {
     const messages = [
-        "MR utan remiss",
-        "7-12 dagar väntetid på MR",
         "Legitimerad naprapat",
         "Specialistläkare inom ortopedi och rygg kirurgi",
-        "Boka tid på hemsidan eller ringa 070-582 21 42"
+        "Boka tid på hemsidan eller ringa 070-582 21 42",
+        "MR utan remiss",
+        "7-12 dagar väntetid på MR",
     ];
 
     const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+    const [show, setShow] = useState(true);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentMessageIndex((prevIndex) => (prevIndex + 1) % messages.length);
-        }, 2800); // Change message every 3 seconds
+            setShow(false); // Start fade out
+
+            setTimeout(() => {
+                setCurrentMessageIndex((prevIndex) => (prevIndex + 1) % messages.length);
+                setShow(true); // Fade in the next message
+            }, 500); // Wait for the fade out to complete before changing the text
+        }, 5500); // 3 seconds visible + 0.5 seconds for fade out
 
         return () => clearInterval(interval);
     }, [messages.length]);
 
     return (
-        <div className="banner ">
-            <p className="banner-text">{messages[currentMessageIndex]}</p>
+        <div className="banner">
+            <p className={`banner-text ${show ? 'show' : ''}`}>{messages[currentMessageIndex]}</p>
         </div>
     );
 };
 
 export default Banner;
+
